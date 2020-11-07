f21202f034b8c4060b1b9d301236d865df3ca8a7
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2020-11-07 17:56:49 +0700

    Validasi code praktikum

--- RentalKendaraan_101/Models/Customer.cs
+++ RentalKendaraan_101/Models/Customer.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan_101.Models
 {
     public partial class Customer
     {
         public int IdCustomer { get; set; }
+        [Required(ErrorMessage = "Nama Customer tidak boleh kosong")]
+        [DisplayName("Nama Customer")]
         public string NamaCustomer { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya boleh diisi oleh angka")]
+        [DisplayName("NIK")]
+        [Required(ErrorMessage = "NIK tidak boleh kosong")]
         public string Nik { get; set; }
+        [Required(ErrorMessage = "Alamat tidak boleh kosong")]
         public string Alamat { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya boleh diisi oleh angka")]
+        [Required(ErrorMessage = "No Hp tidak boleh kosong")]
+        [DisplayName("No HP")]
         public string NoHp { get; set; }
+        [DisplayName("Id Gender")]
+        [Required(ErrorMessage = "Id Gender tidak boleh kosong")]
         public int? IdGender { get; set; }
 
         public Gender Gender { get; set; }
+        [Required(ErrorMessage = "Tanggal peminjaman wajib diisi wajib diisi")]
         public Peminjaman Peminjaman { get; set; }
     }
 }
--- RentalKendaraan_101/Models/Kendaraan.cs
+++ RentalKendaraan_101/Models/Kendaraan.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan_101.Models
 {
     public partial class Kendaraan
     {
         public int IdKendaraan { get; set; }
+        [Required(ErrorMessage = "Nama Kendaraan tidak boleh kosong")]
         public string NamaKendaraan { get; set; }
+        [MinLength(1, ErrorMessage = "No Polisi Minimal 1 angka")]
+        [MaxLength(4, ErrorMessage = "No Polisi Maximal 4 angka")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya boleh diisi oleh angka")]
+        [Required(ErrorMessage = "No Polisi wajib diisi")]
         public string NoPolisi { get; set; }
+        [MinLength(1, ErrorMessage = "No STNK Minimal 1 angka")]
+        [MaxLength(4, ErrorMessage = "No STNK Maximal 4 angka")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya boleh diisi oleh angka")]
         public string NoStnk { get; set; }
         public int? IdJenisKendaraan { get; set; }
+        [Required(ErrorMessage = "Ketersedian wajib diisi")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya boleh diisi oleh angka")]
         public string Ketersediaan { get; set; }
 
         public JenisKendaraan IdKendaraanNavigation { get; set; }
